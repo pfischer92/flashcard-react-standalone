3aaa2844d7b0d35281647a606a8a415d472e37ab
Patrick Fischer <patrick.fischer.privat@gmail.com>
2018-11-11 20:14:51 +0100

    init

---
+++ src/app/Footer.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+
+export default class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    }
+
+    this.tick = this.tick.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  tick(){
+    this.setState({ counter: this.state.counter + 1 });
+  }
+  reset(){
+  }
+  componentDidMount(){
+    setInterval(this.tick, 1000);
+  }
+  componentWillUnmount(){
+    setInterval(null, 0);
+  }
+  render() {
+    const nrStyle = {
+      textAlign: 'center',
+      fontSize: '320px',
+      margin: '10px'
+    };
+    return (
+      <div className="container-fluid">
+        <p style={nrStyle}>{this.state.counter}</p>
+          <Button bsStyle="warning" onClick={this.reset}>
+          Refresh
+        </Button>&nbsp;
+          {this.props.message}
+      </div>
+    )
+  }
+}
