c83d7fa62e563e364192597886bc658e7867a4e9
Patrick Fischer <patrick.fischer.privat@gmail.com>
2018-11-24 10:07:09 +0100

    AB Show dialog with modal

--- src/questionnaire/QuestionnaireTableElement.js
+++ src/questionnaire/QuestionnaireTableElement.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import QuestionnaireShowDialog from './QuestionnaireShowDialog'
 
 const QuestionnaireTableElement = ({ questionnaire }) => (
     <tr key={ questionnaire.id } >
         <td>{ questionnaire.id }</td>
         <td>{ questionnaire.title }</td>
         <td>{ questionnaire.description }</td>
+        <td><QuestionnaireShowDialog questionnaire={ questionnaire } /></td>
     </tr>
 )
 
